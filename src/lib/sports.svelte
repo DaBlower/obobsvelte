116bd6219e9ae09ea868feee73487a9ed04e101c
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-07-28 00:11:09 +1000

    add initial landing page

---
+++ src/lib/sports.svelte
@@ -0,0 +1,17 @@
+<script>
+    import sports from '../sports.json';
+</script>
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center mx-auto">
+    {#each sports as sport}
+        <a href="{sport.link}">
+            <div class="bg-white rounded-lg shadow-md p-4 hover:scale-105 transition-transform text-black">
+                <div class="h-1/2 w-full">
+                    <img src="{sport.image}" alt="A image of the sport {sport.name}">
+                </div>
+                <div class="h-1/2 w-full flex items-center justify-center">
+                    <p class="text-lg text-center">{sport.name}</p>
+                </div>
+            </div>
+        </a>
+    {/each}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Sports from '$lib/sports.svelte';
+</script>
+
+<div class="bg-blue-600 text-white text-center">
+    <h1 class="text-3xl">Hi, I'm Oliver!</h1>
+    <p class="text-xl">Welcome to my personal website!</p>
+    <p>Here you can find stuff about me, what I like and my projects</p>
+
+    <div class="flex justify-center p-8">
+        <button class="bg-white  text-blue-600 font-bold p-4 rounded hover:scale-105">
+            <a href="https://github.com/DaBlower">Check Out my Github</a>
+        </button>
+    </div>
+
+    <div class="flex flex-col items-center px-4">
+        <h1 class="text-xl mb-6">My favourite sports</h1>
+        <div class="max-w-6xl w-full">
+            <Sports />
+        </div>
+    </div>
+</div>
+
+<footer class="text-center p-10 flex justify-center bg-blue-600 text-white">
+    Made with <span class="hover:animate-bounce">❤️</span> by obob :D
+</footer>
